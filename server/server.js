90e4e7cbadba16e602dfaf2e128b07eaa43dc8f9
Palmer-Hatch <56094431+palmermexico@users.noreply.github.com>
2022-03-16 14:39:43 -0600

    initializin

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+//Middleware
+app.use(express.static(path.join(__dirname, "../public")))
+
+
+//Heroku port
+const port = process.env.PORT || 4005
+
+app.listen(port, ()=>{
+    console.log(`Autobots rollin on port ${port}`)
+})
