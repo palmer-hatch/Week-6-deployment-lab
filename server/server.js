a1d20e2ae4baccdddb6a7279cd7af0ca99ddabc7
Palmer-Hatch <56094431+palmermexico@users.noreply.github.com>
2022-03-17 14:19:45 -0600

    git that one in there

--- server/server.js
+++ server/server.js
@@ -6,6 +6,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '6c87700e391141e69b17cbd1f8acfc09',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
+
 //Middleware
 app.use(express.static(path.join(__dirname, "../public")))
 
